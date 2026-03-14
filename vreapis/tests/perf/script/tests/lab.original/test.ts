80fb34ce9fe96fee8efdcfca80dca44c367db848
Andy Bayer Roswell <andyroswellrr@gmail.com>
2026-03-14 21:52:24 +0100

    sample test passed default timeout: 15s

--- vreapis/tests/perf/script/tests/lab.original/test.ts
+++ vreapis/tests/perf/script/tests/lab.original/test.ts
@@ -3,11 +3,11 @@ import { test, expect } from '@playwright/test';
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:8888/lab');
   const main_sidebar = page.getByRole('complementary', { name: 'main sidebar' });
-  // await main_sidebar.waitFor({ state: "visible" })
-  const file_browser_icon = main_sidebar.getByRole('tab').filter({ has: page.locator(`[data-id="filebrowser"]`) });
+  const Running_Terminals_and_Kernels_icon = main_sidebar.locator(`[data-id="jp-running-sessions"]`);
+  await Running_Terminals_and_Kernels_icon.click();
+  const file_browser_icon = main_sidebar.locator(`[data-id="filebrowser"]`);
   await file_browser_icon.click();
   const file_browser = page.getByRole('region', { name: 'File Browser Section' });
-  // await file_browser.waitFor({ state: "visible" })
   const home_dir_icon = file_browser.locator(`[data-icon="ui-components:folder"]`).first();
   await home_dir_icon.click();
 })
