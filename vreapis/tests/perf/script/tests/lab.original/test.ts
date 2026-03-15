92c6a199e547f29d5f031d72f45fa6a3bef9fa0a
Andy Bayer Roswell <andyroswellrr@gmail.com>
2026-03-15 01:50:20 +0100

    refactored sample test

--- vreapis/tests/perf/script/tests/lab.original/test.ts
+++ vreapis/tests/perf/script/tests/lab.original/test.ts
@@ -1,19 +1,24 @@
 import { test, expect } from '@playwright/test'
 
+var main_sidebar
+var file_browser_icon
+var file_browser_section
+var home_dir_icon
+
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:8888/lab')
-  const main_sidebar = page.getByRole('complementary', { name: 'main sidebar' })
-  const Running_Terminals_and_Kernels_icon = main_sidebar.locator(`[data-id="jp-running-sessions"]`)
-  await Running_Terminals_and_Kernels_icon.click()
-  const file_browser_icon = main_sidebar.locator(`[data-id="filebrowser"]`)
-  await file_browser_icon.click()
-  const file_browser = page.getByRole('region', { name: 'File Browser Section' })
-  const home_dir_icon = file_browser.locator(`[data-icon="ui-components:folder"]`).first().locator('path') // locate the only child with click event handler
-  await page.waitForTimeout(5_000)
-  await home_dir_icon.click()
+  await page.goto('http://localhost:8888/lab') // Use the local JupyterLab instance to reduce measuring errors
+  main_sidebar = page.getByRole('complementary', { name: 'main sidebar' })
+  const Running_Terminals_and_Kernels_sidebar_icon = main_sidebar.locator(`[data-id="jp-running-sessions"]`)
+  await Running_Terminals_and_Kernels_sidebar_icon.click() // Click elsewhere on the main sidebar before the click on file browser icon to make the latter click catchable by playwright
+  file_browser_icon = main_sidebar.locator(`[data-id="filebrowser"]`)
+  file_browser_icon.click()
+  file_browser_section = page.getByRole('region', { name: 'File Browser Section' }) // Locate the file browser
+  home_dir_icon = file_browser_section.locator(`[data-icon="ui-components:folder"]`).first().locator('path') // Locate the only child with click event handler
+  await page.waitForTimeout(5_000) // Just for debug purposes
+  await home_dir_icon.click() // Go back to the home directory
 })
 
 test('rmd/D1', async ({ page }) => {
   await expect(page).toHaveTitle('JupyterLab')
-  await page.waitForTimeout(10_000)
+  await page.waitForTimeout(5_000)
 })
