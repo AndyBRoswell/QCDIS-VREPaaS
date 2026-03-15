d7dacf1d029d4a746cbdd31406260b00eafe2d37
Andy Bayer Roswell <andyroswellrr@gmail.com>
2026-03-15 21:33:30 +0100

    refactoring sample test

--- vreapis/tests/perf/script/tests/lab/test.ts
+++ vreapis/tests/perf/script/tests/lab/test.ts
@@ -1,17 +1,23 @@
-import { test, expect, Page, Locator } from '@playwright/test'
+import { test, expect, type Page, type Locator } from '@playwright/test'
 import log from 'loglevel'
+import Node_Path from 'node:path'
+
+type Segmented_Path = string[]
 
 log.setLevel('info')
 
 const test_root: string = 'tmp/rmd'
 
 class File_Browser_Manipulator {
+  public action_delay = 1_000
+
   public page: Page
   public main_sidebar!: Locator
   public file_browser_tab!: Locator
   public file_browser_icon!: Locator
   public file_browser_section!: Locator
   public home_dir_icon!: Locator
+  public path_indicator!: Locator
   public file_list!: Locator
 
   public constructor(page: Page) {
@@ -26,11 +32,17 @@ class File_Browser_Manipulator {
     this.file_browser_section = this.page.getByRole('region', { name: 'File Browser Section' }) // Locate the file browser
     this.file_list = this.file_browser_section.locator(`ul`) // Get the file list for tests
     this.home_dir_icon = this.file_browser_section.locator(`[data-icon="ui-components:folder"]`).first().locator('path')
+    this.path_indicator = this.home_dir_icon.locator(`xpath=../../..`)
+  }
+
+  public async action_with_delay(action: () => Promise<any>, delay: number = this.action_delay) {
+    await action()
+    await this.page.waitForTimeout(delay)
   }
 
   public async toggle() {
     log.info(`Toggling the file browser tab...`)
-    await this.file_browser_icon.click()
+    await this.action_with_delay(async () => await this.file_browser_icon.click(), 250)
     log.info('File browser tab clicked')
   }
 
@@ -41,24 +53,44 @@ class File_Browser_Manipulator {
     return r
   }
 
+  public async current_directory(): Promise<string> {
+    while (await this.visible() === false) { await this.toggle() }
+    log.info(`Getting the current directory...`)
+    const r: string = await this.path_indicator.textContent() as string
+    log.info(r)
+    return r
+  }
+
+  public segmented_path(path: string): Segmented_Path { return path.split(Node_Path.sep).filter(Boolean) }
+
+  public identical(p: Segmented_Path, q: Segmented_Path): boolean {
+    return p.length === q.length && p.every((value, index) => value === q[index])
+  }
+
   public async go_home() {
     while (await this.visible() === false) { await this.toggle() }
     log.info('Going back home...')
-    await this.home_dir_icon.click()
-    log.info('Home dir icon clicked')
+    do {
+      await this.action_with_delay(async () => await this.home_dir_icon.click())
+      log.info('Home dir icon clicked')
+    } while (await this.current_directory() !== '/')
   }
 
   public async goto(path: string, home_as_relative_root: boolean = false): Promise<void> {
-    log.info(`Dest: ${path}`)
-    const path_segments = path.split('/') // `/` as default path segment separator [work on Windows, Linux and macOS]
+    log.info(`Dest: ${ path }`)
+    const path_segments = this.segmented_path(path)
     if (home_as_relative_root) { await this.go_home() }
+    const target_path: string[] = []
     for (const segment of path_segments) {
-      log.info(`Entering ${segment}...`)
-      const entry = file_browser_manipulator.file_list.locator(`[title^="Name: ${segment}"]`)
-      await entry.dblclick()
-      log.info(`Entered ${segment}`)
+      log.info(`Entering ${ segment }...`)
+      const entry = file_browser_manipulator.file_list.locator(`[title^="Name: ${ segment }"]`)
+      target_path.push(segment)
+      do {
+        await this.action_with_delay(async () => await entry.dblclick())
+      } while (this.identical(this.segmented_path(await this.current_directory()), target_path) === false)
+      log.info(`Entered ${ segment }`)
     }
-    log.info(`Arrived at ${path}`)
+    log.info(`Arrived at ${ path }`)
   }
 }
 
@@ -68,7 +100,6 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:8888/lab') // Use the local JupyterLab instance to reduce measuring errors
   file_browser_manipulator = new File_Browser_Manipulator(page)
   await file_browser_manipulator.init()
-  // await page.waitForTimeout(2_000) // Just for debug purposes
   await file_browser_manipulator.goto(test_root, true)
 })
 
