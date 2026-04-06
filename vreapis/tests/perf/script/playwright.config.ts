e76ed3bf04de2b3126993e44e3d22719bce08133
Andy Bayer Roswell <andyroswellrr@gmail.com>
2026-04-06 02:24:56 +0200

    Make Chromium maximized right after launch

--- vreapis/tests/perf/script/playwright.config.ts
+++ vreapis/tests/perf/script/playwright.config.ts
@@ -32,13 +32,23 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    // launchOptions: {
+    //   args: [ '--start-maximized' ],
+    // },
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        // ...devices['Desktop Chrome'],
+        browserName: 'chromium',
+        launchOptions: {
+          args: ['--start-maximized'],
+        },
+        viewport: null,
+      },
     },
 
     {
