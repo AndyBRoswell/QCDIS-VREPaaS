ca42a5a9a13f107cdb9575e67cb96272968e3ec2
Andy Bayer Roswell <andyroswellrr@gmail.com>
2026-06-11 17:18:56 +0200

    revised test arg type

--- vreapis/tests/perf/script/tests/containerizer_test_args.ts
+++ vreapis/tests/perf/script/tests/containerizer_test_args.ts
@@ -1,26 +1,38 @@
 import * as Util from './util.ts'
 
-export const notebook_args: { [key: Util.Pathname]: Util.Cell_Containerizer_Manipulation_Arguments[] } = {
+export const notebook_test_args: { [key: Util.Pathname]: Util.Cell_Containerizer_Manipulation_Arguments[] } = {
   'D1': [
     {
-      Outputs: { 'w': "Integer", 'x': "Integer", 'y': "Integer", },
-      'Base Image': 'r',
+      actions: [ 'extract', 'create', ],
+      image_args: {
+        Outputs: { 'w': "Integer", 'x': "Integer", 'y': "Integer", },
+        'Base Image': 'r',
+      }
     },
     {
-      Inputs: { 'w': "Integer", },
-      Outputs: { names: 'List', },
-      'Base Image': 'r',
+      actions: [ 'extract', 'create', ],
+      image_args: {
+        Inputs: { 'w': "Integer", },
+        Outputs: { names: 'List', },
+        'Base Image': 'r',
+      }
     },
     {
-      Inputs: { x: "Integer", y: "Integer", names: 'List', },
-      Outputs: { t: "Integer", },
-      Parameters: { param_p: "String", },
-      'Base Image': 'r',
+      actions: [ 'extract', 'create', ],
+      image_args: {
+        Inputs: { x: "Integer", y: "Integer", names: 'List', },
+        Outputs: { t: "Integer", },
+        Parameters: { param_p: "String", },
+        'Base Image': 'r',
+      },
     },
     {
-      Inputs: { t: "Integer", },
-      Parameters: { param_a: "String", },
-      'Base Image': 'r',
+      actions: [ 'extract', 'create', ],
+      image_args: {
+        Inputs: { t: "Integer", },
+        Parameters: { param_a: "String", },
+        'Base Image': 'r',
+      },
     },
   ],
 }
