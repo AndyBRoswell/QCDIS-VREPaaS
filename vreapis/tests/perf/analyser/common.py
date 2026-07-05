0acae2ed368b702c972d64fa7ae903999c4d7c6c
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-05 17:56:35 +0200

    print stats of CPU/mem utils

--- vreapis/tests/perf/analyser/common.py
+++ vreapis/tests/perf/analyser/common.py
@@ -2,9 +2,15 @@ import re
 import sys
 import pathlib
 import logging
+import pandas
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='[%(asctime)s.%(msecs)03d] [%(levelname)s] [%(name)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')  # Default logger level: info
 
 RE_flags = re.IGNORECASE
 
 source_dir = pathlib.Path('.log')
+
+pandas.set_option('display.max_columns', None)
+pandas.set_option('display.max_rows', None)
+pandas.set_option('display.width', None)
+pandas.set_option('display.max_colwidth', None)
---
+++ vreapis/tests/perf/analyser/stat_utils.py
@@ -0,0 +1,59 @@
+import os
+import logging
+import pathlib
+import re
+import pandas
+import numpy
+from collections import namedtuple
+import common
+
+supported_platforms: list[str] = ['lab.original', 'lab.vreapi', 'rstudio']
+RE_platform_suffix: re.Pattern[str] = re.compile('|'.join(re.escape(platform) for platform in supported_platforms) + '$', common.RE_flags)
+
+export_dir = pathlib.Path('export/util')
+
+ave_columns: list[str] = []
+for prefix in ['ave:CPU:', 'ave:mem:']:
+    for platform in supported_platforms:
+        ave_columns.append(f'{prefix}{platform}')
+
+logging.info('Start statisticization')
+pathname_prefices: list[str] = []
+File_Info = namedtuple('File_Info', ['pathname_prefix', 'platform'])
+file_info: dict[str, File_Info] = {}
+for csv_pathname in common.source_dir.rglob('*.util.cooked.csv'):
+    csv_pathname_str = str(csv_pathname)
+    pathname_prefix_with_platform_suffix: str = csv_pathname_str[len(str(common.source_dir) + os.pathsep):len(csv_pathname_str) - len('.util.cooked.csv')]
+    platform_match: re.Match | None = RE_platform_suffix.search(pathname_prefix_with_platform_suffix)
+    if not platform_match:
+        logging.warning(f"Skipped CSV pathname with unsupported platform suffix: {csv_pathname}")
+        continue
+    platform: str = platform_match.group(0)
+    pathname_prefix: str = pathname_prefix_with_platform_suffix[:platform_match.start() - len('.')]
+    pathname_prefices.append(pathname_prefix)
+    file_info[csv_pathname_str] = File_Info(pathname_prefix, platform)
+pathname_prefices = list(dict.fromkeys(pathname_prefices))  # keep insertion order
+
+placeholders = numpy.full((len(pathname_prefices), len(ave_columns)), numpy.nan, dtype=numpy.float64)
+ave = pandas.DataFrame(placeholders, index=pathname_prefices, columns=ave_columns)
+
+for csv_pathname in common.source_dir.rglob('*.util.cooked.csv'):
+    csv_pathname_str = str(csv_pathname)
+    table: pandas.DataFrame = pandas.read_csv(csv_pathname)
+    CPU_column_names: list[str] = []
+    mem_column_names: list[str] = []
+    for col in table.columns:
+        match col[0:len('xxx:')]:
+            case 'CPU:':
+                CPU_column_names.append(col)
+            case 'mem:':
+                mem_column_names.append(col)
+            case _:
+                continue
+    ave_CPU_usage = table[CPU_column_names].mean().sum()  # Average CPU usage across all processes and all time points
+    ave_mem_usage = table[mem_column_names].mean().sum()
+    ave.at[file_info[csv_pathname_str].pathname_prefix, f'ave:CPU:{file_info[csv_pathname_str].platform}'] = ave_CPU_usage
+    ave.at[file_info[csv_pathname_str].pathname_prefix, f'ave:mem:{file_info[csv_pathname_str].platform}'] = ave_mem_usage
+ave = ave.round(1)
+
+print(ave)
